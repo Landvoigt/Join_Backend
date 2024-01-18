439e4f11fee0527adf1ba5e97d6c619b956fc485
Landvoigt <tim.voigt@ik.me>
2024-01-18 12:52:06 +0100

    added cors-headers

--- Join_Backend/settings.py
+++ Join_Backend/settings.py
@@ -31,6 +31,10 @@ ALLOWED_HOSTS = [
     '127.0.0.1',
 ]
 
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:4200',
+]
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -47,6 +51,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddelware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
