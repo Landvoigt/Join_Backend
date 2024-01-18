8d8453e1d77941071b28ed4f4767982adc7deca2
Landvoigt <tim.voigt@ik.me>
2024-01-18 17:45:20 +0100

    updated contact admin

--- contacts/admin.py
+++ contacts/admin.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from .models import Contact
 
 class ContactAdmin(admin.ModelAdmin):
-    fields = ('id','first_name', 'last_name','initials','email','phone','color')
-    list_display = ('id','first_name', 'last_name','email','phone',)
+    fields = ('first_name', 'last_name','initials','email','phone','color',)
+    list_display = ('first_name', 'last_name','email','phone',)
     search_fields = ('first_name', 'last_name','email',)
 
 admin.site.register(Contact, ContactAdmin)
